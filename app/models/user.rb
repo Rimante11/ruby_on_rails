0d2a6dddac0fc3858349a9f53476894ca4fcd132
Rimante11 <storulike@gmail.com>
2023-01-31 14:42:07 +0100

    working with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #upper letter or small letter saving
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, 
             uniqueness: { case_sensetive: false }, length: { minimum: 3, maximum: 25 } 
@@ -6,6 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, 
             uniqueness: { case_sensetive: false },  length: { maximum: 105 },
             format: {with: VALID_EMAIL_REGEX } 
+  has_secure_password
 end
 
 # uniqueness: { case_sensetive: false }, för uniq username, även bokstaver Aaa=aaa
