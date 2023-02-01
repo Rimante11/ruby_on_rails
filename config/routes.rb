dedf7c78fe6882f731baf75827705f26151dabfc
Rimante11 <storulike@gmail.com>
2023-02-01 13:31:28 +0100

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   #post 'users', to: 'users#create' #"same as line under"
   resources :users, except: [:new] 
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
