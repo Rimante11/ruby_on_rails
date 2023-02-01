143865aae39491eebd6821b605708fe7ced4e08e
Rimante11 <storulike@gmail.com>
2023-02-01 08:43:26 +0100

    user edit/update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,7 +24,6 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
-   
         # format.html { render :new, status: :unprocessable_entity }
         # format.json { render json: @article.errors, status: :unprocessable_entity }
    end
