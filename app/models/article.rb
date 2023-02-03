4d6e619239b54c10e387b4550ff10f2f601536a2
Rimante11 <storulike@gmail.com>
2023-02-03 15:24:31 +0100

    added association form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 #Article - name of the model, ApplicationRecord - subclass
 class Article < ApplicationRecord
   belongs_to :user #article can belong to one user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100}
   validates :description, presence: true, length: { minimum: 10, maximum: 100}
 end
